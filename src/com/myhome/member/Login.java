88decd7a1847d3373b6079780647b1c06b7c514d
신상준 <93465221+OL89@users.noreply.github.com>
2021-10-31 19:13:17 +0900

    Update Login.java

--- src/com/myhome/member/Login.java
+++ src/com/myhome/member/Login.java
@@ -28,23 +28,23 @@ public class Login extends HttpServlet {
 		boolean rememberMe = Boolean.parseBoolean(request.getParameter("remember_me")); 
 		if(rememberMe) {
 			Cookie cookie = new Cookie("rememberId", id);
-			cookie.setMaxAge(60 * 60 * 24 * 365); // 1년
+			cookie.setMaxAge(60 * 60 * 24 * 365); 
 			cookie.setPath("/");
 			response.addCookie(cookie);
-		} else { // 체크가 안되어있다면, 기존에 있을 수 있는 쿠키를 삭제
-			Cookie cookie = new Cookie("rememberId", null); // rememberId 이름의 임시쿠키 만들기
+		} else {
+			Cookie cookie = new Cookie("rememberId", null); 
 			cookie.setMaxAge(0);
 			cookie.setPath("/");
-			response.addCookie(cookie); // 기존 remeberId 쿠키 덮어쓰기
+			response.addCookie(cookie); 
 		}
 		if(dto != null){
-			//request.setAttribute("nickname", dto.getNickname());
+			
 			HttpSession session = request.getSession();
 			session.setAttribute("currentNickname", dto.getNickname());
 			session.setAttribute("currentId", id);
 		}
 		
-		//pageContext.forward("loginResultView.jsp");
+		
 		RequestDispatcher rd = request.getRequestDispatcher("loginResultView.jsp");
 		rd.forward(request, response);
 	}
