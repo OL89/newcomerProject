4da3d90c8e37f593b92b3986119f62b331b24740
신상준 <93465221+OL89@users.noreply.github.com>
2021-11-02 15:30:44 +0900

    Update ActionForward.java

--- src/com/myhome/board/action/ActionForward.java
+++ src/com/myhome/board/action/ActionForward.java
@@ -1,8 +1,8 @@
 package com.myhome.board.action;
 
-public class ActionForward { // 페이지의 다음 실행 페이지가 어디인지 
-	private boolean isRedirect; // true : redirect 할 것이다. false : forward 할 것이다. 
-	private String nextPath; 	// 다음 경로
+public class ActionForward { 
+	private boolean isRedirect; 
+	private String nextPath; 	
 	
 	public boolean isRedirect() {
 		return isRedirect;
