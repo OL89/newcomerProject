633a23b5e01362affa5ceecd59e92df6f0520bc9
신상준 <93465221+OL89@users.noreply.github.com>
2021-11-02 15:30:20 +0900

    Update Action.java

--- src/com/myhome/board/action/Action.java
+++ src/com/myhome/board/action/Action.java
@@ -3,6 +3,6 @@ package com.myhome.board.action;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public interface Action { // Model 구현 시 execute()를 오버라이드해서 사용한다.
+public interface Action { 
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception;
 }
